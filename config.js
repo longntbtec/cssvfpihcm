4cd8cfd4f34eb9eb902fdf4b777190e6c8eb3dad
Nguyễn Thành Long <longnt69@fpt.edu.vn>
2026-06-04 20:32:16 +0700

    Change Supabase URL and key in config.js
    Updated Supabase URL and key for configuration.

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js
 // Lưu trữ các thông tin cấu hình nhạy cảm hoặc cấu hình môi trường
-const supabaseUrl = 'https://moahaaubepvvpkukjoun.supabase.co';
+const supabaseUrl = 'https://cxswugwyywzovzpurhch.supabase.co';
 // WARNING: Điền Anon Key vào thay cho process.env.SUPABASE_KEY vì file HTML chạy trên trình duyệt client
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1vYWhhYXViZXB2dnBrdWtqb3VuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYzMjk2NTksImV4cCI6MjA5MTkwNTY1OX0.AgY0-QhMeTXF08eCBRnIxyAjq8mtNHYQBDr6F5jqPcQ';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN4c3d1Z3d5eXd6b3Z6cHVyaGNoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAzOTg4MzEsImV4cCI6MjA5NTk3NDgzMX0.vXoAR4_7Yi9KfQnhQwigXntAtgsvbteM6_XdOQDevQs'
